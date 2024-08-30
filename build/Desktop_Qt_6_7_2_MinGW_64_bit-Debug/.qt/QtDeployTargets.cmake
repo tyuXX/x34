109433665f75904704abf6e8d1e7c32ccec24d96
Gokalp Sen <gokalpsen09@gmail.com>
2024-08-30 12:29:51 +0300

    init

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_x34_FILE F:/pROjects/x34/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/x34.exe)
+set(__QT_DEPLOY_TARGET_x34_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_x34_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
